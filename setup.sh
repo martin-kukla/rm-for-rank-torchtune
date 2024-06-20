62288f3f21cb9f0b99442fcdbe76db9745f51da7
Martin Kukla <martin.kukla@cantab.net>
2024-06-20 08:49:59 +0000

    settings for git in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,6 @@ pip install jupyter
 /usr/local/bin/jupyter-notebook --port=3000 --ip=0.0.0.0 --no-browser --allow-root --ServerApp.token= --notebook-dir=/efs/notebooks/mkukla > /root/nb.out 2> /root/nb.err &
 
 # Git
-#git config --global credential.helper cache
+git config --global user.email "martin.kukla@cantab.net"
+git config --global user.name "Martin Kukla"
+git config --global credential.helper store # This is not secure...
